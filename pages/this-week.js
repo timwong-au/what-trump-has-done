571cdeb373a92c1c9281b1c243aa1123d78745f9
Tim <au.timwong@gmail.com>
2025-04-03 18:07:11 +0700

    Fix: Remove unused useEffect import in this-week

--- pages/this-week.js
+++ pages/this-week.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import Head from 'next/head';
 import ActionCard from '../components/ActionCard';
 import Navigation from '../components/Navigation';
